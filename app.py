0c41272cd20e11660fea199f06e2780534bb4fc3
birendra kumar <birendra.developer@gmail.com>
2025-09-28 10:01:13 +0530

    fix error

--- app.py
+++ app.py
@@ -18,28 +18,64 @@ with col1:
     max_value=20,
     value=0,
     step=1,
-    placeholder="Enter a number between 1 and 20")
+    placeholder="Enter a number between 0 and 20")
     
 with col2:
-    Glucose = st.text_input('Glucose Level')
+    Glucose = st.number_input('Glucose Level',
+    min_value=0,
+    max_value=200,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 200")
 
 with col3:
-    BloodPressure = st.text_input('Blood Pressure value')
+    BloodPressure = st.number_input('Blood Pressure value',
+    min_value=0,
+    max_value=200,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 200")
 
 with col1:
-    SkinThickness = st.text_input('Skin Thickness value')
+    SkinThickness = st.number_input('Skin Thickness value',
+    min_value=0,
+    max_value=200,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 200")
 
 with col2:
-    Insulin = st.text_input('Insulin Level')
+    Insulin = st.number_input('Insulin Level',
+    min_value=0,
+    max_value=1000,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 1000")
 
 with col3:
-    BMI = st.text_input('BMI value')
+    BMI = st.number_input('BMI value',
+    min_value=0,
+    max_value=100,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 100")
+    
 
 with col1:
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value',
+    min_value=0,
+    max_value=5,
+    value=0,
+    step=1,
+    placeholder="Enter a number between 0 and 5")
 
 with col2:
-    Age = st.text_input('Age of the Person')
+    Age = st.number_input('Age of the Person',
+    min_value=1,
+    max_value=120,
+    value=1,
+    step=1,
+    placeholder="Enter a number between 1 and 120")
 
 
 # code for Prediction
